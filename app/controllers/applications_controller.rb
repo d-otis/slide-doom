d6391d5fe9411adf571829757eb7771db3eb33b1
d-otis <dan@dan-foley.com>
2020-04-16 17:34:04 -0400

    build post route for /applications

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,13 +1,28 @@
 class ApplicationsController < ApplicationController
 
+	get '/applications' do
+		@applications = Application.all
+
+		erb :'applications/index'
+	end
+
 	get '/applications/new' do
 		@institutions = Institution.all
 
 		erb :'applications/new'
 	end
 
+	get '/applications/:id' do
+
+		erb :'applications/show'
+	end
+
 	post '/applications' do
-		binding.pry
+		application = Application.create(params[:application])
+		application.institution = Institution.find_by(params[:institution])
+		application.save
+
+		redirect "/applications/#{application.id}"
 	end
 
 end
