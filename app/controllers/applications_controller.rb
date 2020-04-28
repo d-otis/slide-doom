0c20e0093e37562d7b667ed84ec7f6a168b5faa5
d-otis <dan@dan-foley.com>
2020-04-28 13:20:22 -0400

    require user to be logged in to access routes in applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,79 +1,109 @@
 class ApplicationsController < ApplicationController
 
 	get '/applications' do
-		@artist = current_user
-		@applications = @artist.applications
+		if logged_in?
+			@artist = current_user
+			@applications = @artist.applications
 
-		erb :'applications/index'
+			erb :'applications/index'
+		else
+			redirect "/login"
+		end
 	end
 
 	get '/applications/new' do
-		@institutions = Institution.all
+		if logged_in?
+			@institutions = Institution.all
 
-		erb :'applications/new'
+			erb :'applications/new'
+		else
+			redirect "/login"
+		end
 	end
 
 	get '/applications/:id' do
-		if current_user.application_ids.include?(params[:id].to_i)
-			@application = Application.find(params[:id])
-			erb :'applications/show'
+		if logged_in?
+			if current_user.application_ids.include?(params[:id].to_i)
+				@application = Application.find(params[:id])
+				erb :'applications/show'
+			else
+				flash[:message] = "Application not found."
+				redirect "/applications"
+			end
 		else
-			flash[:message] = "Application not found."
-			redirect "/applications"
+			redirect "/login"
 		end
 	end
 
 	get '/applications/:id/edit' do
-		if current_user.application_ids.include?(params[:id].to_i)
-			@application = Application.find(params[:id])
-			@institutions = Institution.all
-			erb :'applications/edit'
+		if logged_in?
+			if current_user.application_ids.include?(params[:id].to_i)
+				@application = Application.find(params[:id])
+				@institutions = Institution.all
+				erb :'applications/edit'
+			else
+				flash[:message] = "Application not found."
+				redirect "/applications"
+			end
 		else
-			flash[:message] = "Application not found."
-			redirect "/applications"
+			redirect "/login"
 		end
-
 	end
 
 	post '/applications' do
-		artist = current_user
-		app = Application.new(params[:application])
-		app.artist = current_user
+		if logged_in?
+			artist = current_user
+			app = Application.new(params[:application])
+			app.artist = current_user
 
-		if app.save
-			flash[:message] = "Application successfully created!"
-			redirect "/applications/#{app.id}"
-		else
-			flash[:message] = app.errors.messages.collect do |k, v|
-				"#{k.to_s.capitalize.gsub("_", " ")} #{v.join}"
+			if app.save
+				flash[:message] = "Application successfully created!"
+				redirect "/applications/#{app.id}"
+			else
+				flash[:message] = app.errors.messages.collect do |k, v|
+					"#{k.to_s.capitalize.gsub("_", " ")} #{v.join}"
+				end
+				
+				redirect "applications/new"
 			end
-			
-			redirect "applications/new"
+		else
+			redirect "/login"
 		end
 	end
 
 	patch '/applications/:id' do
 		# guard against URL hacking?
-		app = Application.find(params[:id])
-		if app.update(params[:application])
-			flash[:message] = "Successfully updated application."
+		if logged_in?
+			app = Application.find(params[:id])
+			if app.update(params[:application])
+				flash[:message] = "Successfully updated application."
 
-			redirect "applications/#{app.id}"
-		else
-			flash[:message] = app.errors.messages.collect do |k, v|
-				"#{k.to_s.capitalize.gsub("_", " ")} #{v.join}"
+				redirect "applications/#{app.id}"
+			else
+				flash[:message] = app.errors.messages.collect do |k, v|
+					"#{k.to_s.capitalize.gsub("_", " ")} #{v.join}"
+				end
+
+				redirect "/applications/#{app.id}/edit"
 			end
-
-			redirect "/applications/#{app.id}/edit"
+		else
+			redirect "/login"
 		end
 	end
 
 	delete '/applications/:id' do
-		# guard against URL hacking?
-		application = Application.find(params[:id])
-		application.delete
+		if logged_in?
+			application = Application.find(params[:id])
+			if current_user.applications.include?(application)
+				application.delete
+			else
+				flash[:message] = "Application not found."
+			end
 
-		redirect '/applications'
+			redirect '/applications'
+		else
+			redirect "/login"
+		end
 	end
 
 end
