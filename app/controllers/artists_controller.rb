0ee00c5443161a57560adcd0311788a0b7818fff
d-otis <dan@dan-foley.com>
2020-04-28 14:31:55 -0400

    remove artist index route

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,5 @@
 class ArtistsController < ApplicationController
 
-	get '/artists' do
-		@artists = Artist.all
-
-		erb :'/artists/index'
-	end
-
 	get '/register' do
 		erb :'/artists/register'
 	end
