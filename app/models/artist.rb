6e80ed82b044f7a65726c10238af7d992a24ad66
d-otis <dan@dan-foley.com>
2020-04-28 12:17:38 -0400

    dry up custom slug code

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,3 @@
-# require_relative "./concerns/slugify"
-
 class Artist < ActiveRecord::Base
 	include SlugMod::Slugify
 	extend SlugMod::ClassMethods
@@ -9,12 +7,4 @@ class Artist < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: true
-
-	# def slug
-	# 	name.downcase.gsub(/[!?@ +,&]/, "-").gsub(/[().']/, "").gsub("$", "s").squeeze("-")
-	# end
-
-	# def self.find_by_slug(slug)
-	# 	self.all.find {|instance| instance.slug == slug} 
-	# end
 end
